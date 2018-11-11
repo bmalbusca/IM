0ecb51ba35d4d35b289802f17bd4adea3e563503
bmalbusca <brunocfigueiredo@tecnico.ulisboa.pt>
2018-11-11 11:34:54 +0000

    media pesada para medir frequencias

--- meas_freq.m
+++ meas_freq.m
@@ -1,13 +1,30 @@
-function [freqs spectrum]= meas_freq( S, t,fs,n)
+function [freqs spectrum mfreq]= meas_freq( S, t,fs,n)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 freqs = fs*ceil((0:(n/2)-1))/n;
-ff = abs(fft(S) / n);
+ff = abs(fft(S)) / n;
 % All the values shall be duplicated, except the first and last values.
 spectrum = ff(1:n / 2) * 2;
 figure();
-plot(freqs',spectrum)
+plot(freqs',spectrum)   
 
+deltaf = fs/n;
+[~,index]= max(spectrum);
+mfreq = deltaf * index
+
+res = 0;
+div = 0;
+j = index -3 ; 
+
+while j< index+3 
+    
+    res = res + (spectrum(j)* (j*deltaf));
+    div = div +spectrum(j) ;  
+    j=j + 1; 
 end
+
+freq_pesada = res/div
+
+
 
